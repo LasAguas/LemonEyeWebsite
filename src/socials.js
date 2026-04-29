255a21d22b26507e74c199e3c660101fea989160
LasAguas <lasaguasproductions@gmail.com>
2026-04-29 08:40:17 +0200

    Out of Reach Update

--- src/socials.js
+++ src/socials.js
@@ -19,11 +19,15 @@ const gigs = [
   },
 ]
 
+const outOfReachPlatforms = [
+  { label: 'Apple Music', url: 'https://music.apple.com/gb/album/out-of-reach-single/1892339999' },
+  { label: 'Tidal', url: 'https://tidal.com/album/514506666/u' },
+  { label: 'YouTube Music', url: 'https://www.youtube.com/watch?v=V2_Sft3qiOU&list=RDV2_Sft3qiOU&start_radio=1' },
+  { label: 'Deezer', url: 'https://link.deezer.com/s/337SaFfzcsYOXVH0OPDJ6' },
+  { label: 'Spotify', url: 'https://open.spotify.com/album/2rCkRsHw14IPHbVmbTab5x' },
+]
+
 const links = [
-  {
-    label: 'Presave "Out of Reach"',
-    url: 'https://distrokid.com/hyperfollow/lemoneye/out-of-reach?ref=release',
-  },
   {
     label: 'Mailing list',
     url: 'http://eepurl.com/jomA7I',
@@ -78,6 +82,21 @@ const gigHTML = nextGig ? `
   </div>
 `
 
+const outOfReachHTML = `
+  <div class="listen-expand" data-expanded="false">
+    <button type="button" class="gig-button listen-expand__toggle" aria-expanded="false" aria-controls="oor-platforms">
+      Listen to "Out of Reach" <span class="listen-expand__chev" aria-hidden="true">&darr;</span>
+    </button>
+    <div id="oor-platforms" class="listen-expand__panel" hidden>
+      ${outOfReachPlatforms.map(p => `
+        <a class="gig-button gig-button--platform" href="${p.url}" target="_blank" rel="noopener">
+          ${p.label} &rarr;
+        </a>
+      `).join('')}
+    </div>
+  </div>
+`
+
 const linksHTML = links.map(l => `
   <a class="gig-button" href="${l.url}" target="_blank" rel="noopener">
     ${l.label} &rarr;
@@ -93,6 +112,7 @@ document.querySelector('#app').innerHTML = `
     </div>
 
     <div class="socials-links" style="display:flex; flex-direction:column; gap:1.25rem; margin-top:4rem;">
+      ${outOfReachHTML}
       ${linksHTML}
     </div>
   </section>
@@ -101,3 +121,15 @@ document.querySelector('#app').innerHTML = `
 `
 
 initMobileMenu()
+
+const expand = document.querySelector('.listen-expand')
+if (expand) {
+  const toggle = expand.querySelector('.listen-expand__toggle')
+  const panel = expand.querySelector('.listen-expand__panel')
+  toggle.addEventListener('click', () => {
+    const open = expand.dataset.expanded === 'true'
+    expand.dataset.expanded = open ? 'false' : 'true'
+    toggle.setAttribute('aria-expanded', open ? 'false' : 'true')
+    panel.hidden = open
+  })
+}

0cbb51adfe89f4854894b5e6de4490879dfa3d33
LasAguas <lasaguasproductions@gmail.com>
2026-05-24 15:52:01 +0200

    Footer Links Fixed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,12 @@
 const socials = [
-  { name: 'Apple Music', img: 'Lemon Eye On Apple Music.png', url: '#' },
-  { name: 'Bandcamp', img: 'Lemon Eye On Bandcamp.png', url: '#' },
-  { name: 'Deezer', img: 'Lemon Eye On Deezer.png', url: '#' },
-  { name: 'Facebook', img: 'Lemon Eye On Facebook.png', url: '#' },
+  { name: 'Apple Music', img: 'Lemon Eye On Apple Music.png', url: 'https://music.apple.com/gb/artist/lemon-eye/1663690008' },
+  { name: 'Bandcamp', img: 'Lemon Eye On Bandcamp.png', url: 'https://lemoneye.bandcamp.com/' },
+  { name: 'Deezer', img: 'Lemon Eye On Deezer.png', url: 'https://www.deezer.com/de/artist/196463387' },
+  { name: 'Facebook', img: 'Lemon Eye On Facebook.png', url: 'https://www.facebook.com/profile.php?id=61556828711254' },
   { name: 'Instagram', img: 'Lemon Eye On Instagram.png', url: 'https://www.instagram.com/lemon.eye.zeband/' },
-  { name: 'SoundCloud', img: 'Lemon Eye On Soundcloud.png', url: '#' },
-  { name: 'Spotify', img: 'Lemon Eye On Spotify.png', url: '#' },
-  { name: 'Tidal', img: 'Lemon Eye On Tidal.png', url: '#' },
+  { name: 'SoundCloud', img: 'Lemon Eye On Soundcloud.png', url: 'https://soundcloud.com/lemon-eye' },
+  { name: 'Spotify', img: 'Lemon Eye On Spotify.png', url: 'https://open.spotify.com/artist/0TWRk3ga3JAtCHFyGZFWiT' },
+  { name: 'Tidal', img: 'Lemon Eye On Tidal.png', url: 'https://tidal.com/artist/46840785' },
   { name: 'YouTube', img: 'Lemon Eye On YouTube.png', url: 'https://www.youtube.com/@lemon.eye.zeband' },
 ]
 
